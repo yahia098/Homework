941639a84a5961f173483769c90ba7bcf1747803
yahia <90655976+yahia098@users.noreply.github.com>
2025-02-21 22:38:57 +0100

    Update exercise6.c

--- td4/exercise6.c
+++ td4/exercise6.c
@@ -1,2 +1,41 @@
+#include <math.h>
 #include <stdio.h>
-int main(int argc, char *argv[]) { return 0; }
+#include <stdlib.h>
+float Positive(float a, float b, float c) {
+  if (a <= 0 || b <= 0 || c <= 0)
+    return 0;
+  else
+    return 1;
+}
+int main(int argc, char *argv[]) {
+  switch (argc) {
+  case 1:
+    printf("too little arguments\n");
+    return 1;
+    break;
+  case 2:
+    printf("too little arguments\n");
+    return 1;
+    break;
+  case 3:
+    printf("too little arguments\n");
+    return 1;
+    break;
+
+  case 4:
+    if (!Positive(atof(argv[1]), atof(argv[2]), atof(argv[3]))) {
+      printf("some of the sides are of negative length! \n");
+      return 3;
+    }
+    break;
+  default:
+    printf("too many arguments\n");
+    return 2;
+    break;
+  }
+  float S = (atof(argv[1]) + atof(argv[2]) + atof(argv[3])) / 2;
+  float A =
+      sqrt(S * (S - atof(argv[1])) * (S - atof(argv[2])) * (S - atof(argv[3])));
+  printf("the area of the triangle is : %lf \n", A);
+  return 0;
+}
